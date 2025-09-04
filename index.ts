860109a4add886280b00402335a41edf6d5aa27a
Ania-beigi <anahita.einbeigi@gmail.com>
2025-09-04 20:18:12 +0200

    cors ändring

--- index.ts
+++ index.ts
@@ -21,7 +21,7 @@ const app = express();
 
 // Tillåt frontend på localhost:3000 (ändra till din frontend URL vid deployment)
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://brilino.se'],
+  origin: ['http://localhost:3000', 'https://brilino.se', 'https://www.brilino.se'],
 }));
 
 app.use(express.json());
